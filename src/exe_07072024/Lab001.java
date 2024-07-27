42c66cbac4fd63df3d758b55d885abf4d711ff83
Priyansi <patelpriyansi1806@gmail.com>
2024-07-27 19:40:47 +0530

    All Files Added

--- src/exe_07072024/Lab001.java
+++ src/exe_07072024/Lab001.java
@@ -3,6 +3,6 @@ package exe_07072024;
 public class Lab001
 {
     public static void main(String[] args) {
-        System.out.println("HellO Priyanshi");
+        System.out.println("HellO");
     }
 }
