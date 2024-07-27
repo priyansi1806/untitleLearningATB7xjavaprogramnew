a3d8cd977cb9a3d3cc9ab617f501a94e2e2bd432
Priyansi <patelpriyansi1806@gmail.com>
2024-07-27 20:17:10 +0530

    new files added

---
+++ src/exe_07072024/Lab003.java
@@ -0,0 +1,8 @@
+package exe_07072024;
+
+public class Lab003 {
+    public static void main(String[] priya) {//This is the entry point from where jvm will understand to execute the code
+        System.out.println("priya");
+        System.out.println("priya");
+    }
+}
