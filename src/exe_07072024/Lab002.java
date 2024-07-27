2777f4e1bf81eb74ec51970a13321faec0256efb
Priyansi <patelpriyansi1806@gmail.com>
2024-07-27 19:28:53 +0530

    All Files Added

--- src/exe_07072024/Lab002.java
+++ src/exe_07072024/Lab002.java
@@ -4,8 +4,6 @@ public class Lab002
 {
     public static void main(String[] args) {
         System.out.println(2+2);//Single command we used to explain your junior team member to explain the code
-        System.out.println(2-2);
-        System.out.println(2/2);
         System.out.println(2*2);
         System.out.println(3/2);
     }
